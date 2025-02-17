33eabdc88634661e4d5fde4f0f2598480cc4c7e7
Amit-103 <amit.roy@zupee.in>
2025-02-17 17:08:57 +0530

    main

--- git.js
+++ git.js
@@ -1,10 +1,3 @@
-const b = "afadf";
-
-const b = "ff";
-const l = "kkkk";
-const aa = " asdfjl;kdf";
-const vval = "Hello world";
-
-const hw = "hiiiii";
-
-const a = "kkkdlf";
+const a = "a";
+const b = "b";
+const c = "c";
